486cbe45742a28d0357ce2a30f43131b962a3d1a
Japnoor2104 <sjapnoor11@gmail.com>
2022-07-28 14:43:38 +0530

    change

--- app/src/main/java/com/japnoor/relativelayoutassignment/MainActivity.kt
+++ app/src/main/java/com/japnoor/relativelayoutassignment/MainActivity.kt
@@ -2,10 +2,15 @@ package com.japnoor.relativelayoutassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var swOneWay : Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
